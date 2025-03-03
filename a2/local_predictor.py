8268927c92e6bf497262e249543102896e8e2de1
Matthew Chan <mattchan876@gmail.com>
2025-03-02 23:29:09 -0500

    A2 Simulation Code

---
+++ a2/local_predictor.py
@@ -0,0 +1,97 @@
+import m5
+from m5.objects import *
+
+import argparse
+
+class L1ICache(Cache):
+    assoc = 2
+    tag_latency = 1
+    data_latency = 1
+    response_latency = 1
+    mshrs = 8
+    tgts_per_mshr = 20
+    size='128B'
+
+class L1DCache(Cache):
+    assoc = 2
+    tag_latency = 1
+    data_latency = 1
+    response_latency = 1
+    mshrs = 8
+    tgts_per_mshr = 20
+    size='4kB'
+
+## Add the "binary" option to the script
+DEFAULT_BINARY = '/u/csc368h/winter/pub/workloads/hello'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('binary', type=str, default=DEFAULT_BINARY)
+parser.add_argument('-a', '--binary_args')
+parser.add_argument('-f', '--frequency', type=str, default='75MHz')
+
+## Parse command-line arguments
+args = parser.parse_args()
+
+# System creation
+system = System()
+
+## gem5 needs to know the clock and voltage
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = args.frequency
+system.clk_domain.voltage_domain = VoltageDomain() # defaults to 1V
+
+## Create a crossbar so that we can connect main memory and the CPU (below)
+system.membus = SystemXBar()
+system.system_port = system.membus.cpu_side_ports
+
+## Use timing mode for memory modelling
+system.mem_mode = 'timing'
+
+# CPU Setup
+system.cpu = X86TimingSimpleCPU()
+system.cpu.l1d = L1DCache()
+system.cpu.l1d.mem_side = system.membus.cpu_side_ports
+system.cpu.l1d.cpu_side = system.cpu.dcache_port
+
+system.cpu.l1i = L1ICache()
+system.cpu.l1i.mem_side = system.membus.cpu_side_ports
+system.cpu.l1i.cpu_side = system.cpu.icache_port
+
+## This is needed when we use x86 CPUs
+system.cpu.createInterruptController()
+system.cpu.interrupts[0].pio = system.membus.mem_side_ports
+system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
+system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
+
+# Branch Predictor
+system.cpu.branchPred = LocalBP(localPredictorSize=4096, localCtrBits=8)
+
+# Memory setup
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.port = system.membus.mem_side_ports
+
+## A memory controller interfaces with main memory; create it here
+system.mem_ctrl.dram = DDR3_1600_8x8()
+
+## A DDR3_1600_8x8 has 8GB of memory, so setup an 8 GB address range
+address_ranges = [AddrRange('8GB')]
+system.mem_ranges = address_ranges
+system.mem_ctrl.dram.range = address_ranges[0]
+
+# Process setup
+process = Process()
+
+## Use a full path to the binary
+binary = args.binary
+process.cmd = [binary, args.binary_args]
+
+## The necessary gem5 calls to initialize the workload and its threads
+system.workload = SEWorkload.init_compatible(binary)
+system.cpu.workload = process
+system.cpu.createThreads()
+
+# Start the simulation
+root = Root(full_system=False, system=system) # must assign a root
+
+m5.instantiate() # must be called before m5.simulate
+m5.simulate()
---
+++ a2/run_simulations.py
@@ -0,0 +1,152 @@
+import subprocess
+import os
+import shutil
+import glob
+
+DATA = [200000, [2000, 20000], 1000, 5000, 500]
+
+def generate_int_sort(arg):
+    cmd = [
+        "/u/csc368h/winter/pub/workloads/pbbsbench/testData/sequenceData/randomSeq",
+        "-t", "int", str(arg), f"data/random_seq{arg}"
+    ]
+    subprocess.run(cmd)
+
+def generate_spanning_forest(arg1, arg2):
+    cmd = [
+        "/u/csc368h/winter/pub/workloads/pbbsbench/testData/graphData/randLocalGraph",
+        "-d", "3", "-m", str(arg1), str(arg2), f"data/random_graph{arg2}"
+    ]
+    subprocess.run(cmd)
+
+def generate_lrs(arg):
+    cmd = f"tr -dc 'a-z' </dev/urandom | head -c {arg} > data/random_string{arg}"
+    subprocess.run(cmd, shell=True)
+
+def generate_convex_hull(arg):
+    cmd = [
+        "/u/csc368h/winter/pub/workloads/pbbsbench/testData/geometryData/randPoints",
+        "-s", "-d", "2", str(arg), f"data/random_points_2d_{arg}"
+    ]
+    subprocess.run(cmd)
+
+def generate_nbody(arg):
+    cmd = [
+        "/u/csc368h/winter/pub/workloads/pbbsbench/testData/geometryData/randPoints",
+        "-S", "-d", "3", str(arg), f"data/random_points_3d_{arg}"
+    ]
+    subprocess.run(cmd)
+
+
+def run_int_sort(arg, type, cpu):
+    cmd = [
+        "/u/csc368h/winter/pub/bin/gem5.opt",
+        "-d", f"outputs/integer_sort/{type}",
+        cpu,
+        "/u/csc368h/winter/pub/workloads/pbbsbench/benchmarks/integerSort/parallelRadixSort/isort",
+        "--binary_args", f"data/random_seq{arg}"
+    ]
+    subprocess.run(cmd)
+
+def run_sf(arg, type, cpu):
+    cmd = [
+        "/u/csc368h/winter/pub/bin/gem5.opt",
+        "-d", f"outputs/spanning_tree/{type}",
+        cpu,
+        "/u/csc368h/winter/pub/workloads/pbbsbench/benchmarks/spanningForest/ndST/ST",
+        "--binary_args", f"data/random_graph{arg}"
+    ]
+    subprocess.run(cmd)
+
+def run_lrs(arg, type, cpu):
+    cmd = [
+        "/u/csc368h/winter/pub/bin/gem5.opt",
+        "-d", f"outputs/lrs/{type}",
+        cpu,
+        "/u/csc368h/winter/pub/workloads/pbbsbench/benchmarks/longestRepeatedSubstring/doubling/lrs",
+        "--binary_args", f"data/random_string{arg}"
+    ]
+    subprocess.run(cmd)
+
+def run_convex_hull(arg, type, cpu):
+    cmd = [
+        "/u/csc368h/winter/pub/bin/gem5.opt",
+        "-d", f"outputs/convex_hull/{type}",
+        cpu,
+        "/u/csc368h/winter/pub/workloads/pbbsbench/benchmarks/convexHull/quickHull/hull",
+        "--binary_args", f"data/random_points_2d_{arg}"
+    ]
+    subprocess.run(cmd)
+
+def run_nbody(arg, type, cpu):
+    cmd = [
+        "/u/csc368h/winter/pub/bin/gem5.opt",
+        "-d", f"outputs/nbody/{type}",
+        cpu,
+        "/u/csc368h/winter/pub/workloads/pbbsbench/benchmarks/nBody/parallelCK/nbody",
+        "--binary_args", f"data/random_points_3d_{arg}"
+    ]
+    subprocess.run(cmd)
+
+
+def setup_data(data):
+    generate_int_sort(data[0])
+    generate_spanning_forest(data[1][0], data[1][1])
+    generate_lrs(data[2])
+    generate_convex_hull(data[3])
+    generate_nbody(data[4])
+
+def run_simulation(data, type, cpu):
+    run_int_sort(data[0], type, cpu)
+    run_sf(data[1][0], type, cpu)
+    run_lrs(data[2], type, cpu)
+    run_convex_hull(data[3], type, cpu)
+    run_nbody(data[4], type, cpu)
+
+def split_stats(input_dir, output_dir):
+    for file in glob.glob(os.path.join(input_dir, "**/stats.txt"), recursive=True):
+        parent_dir = os.path.basename(os.path.dirname(os.path.dirname(file)))
+        subdir = os.path.basename(os.path.dirname(file))
+        
+        interest_dir = os.path.join(output_dir, parent_dir)
+        os.makedirs(interest_dir, exist_ok=True)
+        
+        interest_file = os.path.join(interest_dir, f"{subdir}.txt")
+        
+        with open(file, "r") as f, open(interest_file, "w") as out_f:
+            section = 0
+            for line in f:
+                if "---------- Begin Simulation Statistics ----------" in line:
+                    section += 1
+                elif "---------- End Simulation Statistics   ----------" in line:
+                    continue
+                if section == 2:
+                    out_f.write(line)
+        
+        print(f"Processed: {file} → {interest_file}")
+
+
+
+
+
+def run_all_simulations():
+    # Generate data if needed
+    # setup_data(DATA)
+
+    # Baseline
+    run_simulation(DATA, "basic", "basic_cpu.py")
+
+    # Prefetchers
+    run_simulation(DATA, "stride_prefetcher", "stride_prefetcher.py")
+    run_simulation(DATA, "tagged_prefetcher", "tagged_prefetcher.py")
+
+    # Branch predictors
+    run_simulation(DATA, "local_predictor", "local_predictor.py")
+    run_simulation(DATA, "tournament_predictor", "tournament_predictor.py")
+
+    # Isolate region of interest stats
+    shutil.rmtree("filtered_outputs", ignore_errors=True)
+    split_stats("outputs", "filtered_outputs")
+
+if __name__ == "__main__":
+    run_all_simulations()
